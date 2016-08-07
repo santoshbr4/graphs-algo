bc9058f275308fb3b3d8ba2bf79e15a9874d2134
santoshbr4 <santoshbr4@gmail.com>
2016-08-07 20:53:44 +0530

    Create grid_main.cpp
    This solution gives time limit exceeded

--- grid_main.cpp
+++ grid_main.cpp
@@ -6,37 +6,105 @@
 
 using namespace std;
 
-int q[1000000]; //Sufficiently large BFS queue
 int visited[1000][1000];
-int arr[1000];
+unsigned long routes;
+
+int dx[2] = { 0, 1 };
+int dy[2] = { 1, 0 };
+
+typedef struct node_t
+{
+	int x;
+	int y;
+}node;
+
+node q[1000000]; //Sufficiently large BFS queue
 
 int front = -1, rear = -1;
+int m, n;
+unsigned long K;
 
 bool isEmpty()
 {
 	if (front != rear)
-		return true;
-	else
 		return false;
+	else
+		return true;
+}
+
+bool isValid(int x, int y)
+{
+	if (x > m || y > n || x < 0 || y < 0)
+		return false;
+	else
+		return true;
 }
 
 int find_all_paths()
 {
-	int result = 0;
+	routes = 0;
+	rear = -1;
+	front = -1;
 
-	//BFS
-	q[++rear];
-	while (!isEmpty())
+	for (int i = 0; i < m; i++)
 	{
-		int ele = q[++front];
-		
+		for (int j = 0; j < n; j++)
+		{
+			visited[i][j] = false;
+		}
 	}
 
-	return result;
+	//BFS
+	node beg;
+	beg.x = 0;
+	beg.y = 0;
+
+	q[++rear] = beg;
+
+	while (!isEmpty())
+	{
+		node ele = q[++front];
+		visited[ele.x][ele.y] = true;
+
+		for (int i = 0; i < 2; i++)
+		{
+			int X = ele.x + dx[i];
+			int Y = ele.y + dy[i];
+			if (isValid(X, Y) && !visited[X][Y])
+			{
+				if (X == m && Y == n)
+				{
+					//cout << " Found a route " << endl;
+					routes++;
+				}
+				else
+				{
+					node tmp = { X, Y };
+					q[++rear] = tmp;
+				}
+			}
+		}
+	}
+
+	return routes%K;
 }
 
 int main()
 {
+	//freopen("input.txt", "r", stdin);
+
+	int T = 0;
+	cin >> T;
+
+	for (int test_case = 0; test_case < T; test_case++)
+	{
+		cin >> m;
+		cin >> n;
+		cin >> K;
+
+		int result = find_all_paths();
+		cout << result << endl;
+	}
 
 	return 0;
 }
