9cbe6efafcdc0e465f1b83c8e481dd7ac12573f5
santoshbr4 <santoshbr4@gmail.com>
2018-09-12 08:12:07 +0530

    Create graph.h
    Header file for a graph data structure.

---
+++ graph.h
@@ -0,0 +1,24 @@
+/* Author: Santosh B R (santoshbr4@gmail.com)*/
+#pragma once
+
+typedef struct adjNode_t
+{
+	int vertex;
+	adjNode_t *next;
+}adjNode;
+
+typedef struct adjList_t
+{
+	adjNode *head;
+}adjList;
+
+typedef struct graph_t
+{
+	int v;
+	int *visited;
+	int *processed;
+	adjList *arr;
+}graph;
+
+void bfs(graph *g);
+void dfs(graph *g);
